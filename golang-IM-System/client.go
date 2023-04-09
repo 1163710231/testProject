05d513499b9aa0ce09f1365e3aaa980309273da6
1163710231 <570232054@qq.com>
2023-04-09 23:27:01 +0800

    V0.9.3 客户端实现-菜单显示

--- golang-IM-System/client.go
+++ golang-IM-System/client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	Connection net.Conn
+	Flag       int
 }
 
 func NewClient(serverIP string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIP string, serverPort int) *Client {
 	client := Client{
 		ServerIP:   serverIP,
 		ServerPort: serverPort,
+		Flag:       999,
 	}
 
 	// 连接 Server
@@ -30,6 +32,53 @@ func NewClient(serverIP string, serverPort int) *Client {
 
 	// 返回 Client 对象
 	return &client
+}
+
+func (this *Client) showMenu() bool {
+	fmt.Println("0.退出")
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+
+	var f int
+	_, ScanError := fmt.Scanln(&f)
+	if ScanError != nil {
+		fmt.Println("fmt.Scan Error:", ScanError)
+		return false
+	}
+	if 0 <= f && f <= 3 {
+		this.Flag = f
+		return true
+	} else {
+		fmt.Println("Input Error!")
+		return false
+	}
+}
+
+func (this *Client) Run() {
+	for this.Flag != 0 {
+		for this.showMenu() == false {
+
+		}
+
+		// 根据不同的业务模式处理不同的业务
+		switch this.Flag {
+		case 0: // 退出
+			fmt.Println("退出")
+			break
+		case 1: // 公聊模式
+			fmt.Println("公聊模式")
+			break
+		case 2: // 私聊模式
+			fmt.Println("私聊模式")
+			break
+		case 3: // 更新用户名
+			fmt.Println("更新用户名")
+			break
+		default: // 错误
+			fmt.Println("Model error!")
+		}
+	}
 }
 
 var serverIP string
@@ -53,5 +102,5 @@ func main() {
 	fmt.Println("Successfully connected to the server!")
 
 	// 启动客户端的业务
-	select {}
+	client.Run()
 }
